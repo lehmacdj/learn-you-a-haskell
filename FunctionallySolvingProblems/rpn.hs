607b17468ef71e90ecbf62db9781541ef4fe7a8f
Devin Lehmacher <lehmacdj@gmail.com>
2016-11-25 14:28:23 -0500

    Fix rpn solver

--- FunctionallySolvingProblems/rpn.hs
+++ FunctionallySolvingProblems/rpn.hs
@@ -9,7 +9,6 @@ solveRPN = head . foldl foldingFunction [] . words
 foldingFunction :: (Floating a, Read a) => [Maybe a] -> String -> [Maybe a]
 foldingFunction (x:y:ys) op = ((op `lookup` binary) <*> y <*> x) : ys
 foldingFunction (x:xs) op = ((op `lookup` unary) <*> x) : xs
-foldingFunction xs "sum" = [(liftM sum) $ sequence xs]
 foldingFunction xs num = readMaybe num : xs
 
 unary :: Floating a => [(String, a -> a)]
